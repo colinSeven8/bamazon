ac79b50318c71c01c4cfb2403545767810a08d94
colinSeven8 <ccnorse80@gmail.com>
2019-11-01 13:17:04 -0600

    Initial commit. Set up requirements and conection in js file

---
+++ bamazonCustomer.js
@@ -0,0 +1,10 @@
+let mysql = require('mysql');
+let inquirer = require('inquirer');
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'bamazon_db'
+});
