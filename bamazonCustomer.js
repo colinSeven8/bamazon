525f83a621cf755b1e935bfeaa09f8c6cca582cf
colinSeven8 <ccnorse80@gmail.com>
2019-11-03 18:54:30 -0700

    Started building inquirer,

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,12 @@
 let mysql = require('mysql');
 let inquirer = require('inquirer');
+let Table = require('cli-table');
+
+// Globals
+let table = new Table({
+    head: ['PRODUCT ID', 'PRODUCT NAME', 'DEPARTMENT', 'PRICE', 'QUANTITY'],
+    colWidths: [100, 200]
+});
 
 let connection = mysql.createConnection({
     host: 'localhost',
@@ -9,22 +16,51 @@ let connection = mysql.createConnection({
     database: 'bamazon_db'
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     promptUserInput();
 });
 
-function promptUserInput() {
-    inquirer.prompt({
-        type: "list",
-        name: "menu",
-        msg: "What would like to do?",
-        choice: [
-            "a",
-            "b",
-            "c"
-        ]
-    }).then(function(answer) {
+function readAndDisplayProducts() {
 
+    let query = "SELECT * FROM products";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            table.push(
+                { id: res[i].item_id },
+                { name: res[i].product_name },
+                { dept: res[i].department_name },
+                { price: res[i].price },
+                { quantity: res[i].stock_quantity }
+            );
+            console.log(table.toString());
+        }
+        promptUserInput();
     });
+}
+
+function promptUserInput() {
+    inquirer.prompt(
+        {
+            type: "input",
+            name: "ID",
+            msg: "What is the ID of the product you would like to buy?"
+        },
+        {
+            type: "input",
+            name: "QUANTITY",
+            msg: "How many would you like to purchase?"
+        }).then(function (answer) {
+
+            for (let i = 0; i < table.length; i++) {
+                if (answer.ID === table[i].id && answer.QUANTITY >= table[i].quantity) {
+                    let total = (table[i].price * answer.QUANTITY).toFixed(2);
+                    console.log('\nThank you for your purchase! Your total is $' + total + '\n');
+                    connection.end();
+                } else {
+                    console.log("Please check the item number and quantity again...");
+                }
+            }
+        });
 }
--- bamazon_db.sql
+++ bamazon_db.sql
@@ -3,17 +3,33 @@ CREATE DATABASE bamason_db;
 
 USE bamazon_db;
 
-CREATE TABLE products (
+CREATE TABLE products
+(
     item_id INT NOT NULL AUTO_INCREMENT,
-    product_name (45) VARCHAR DEFAULT "",
-    department_name (45) VARCHAR DEFAULT "",
+    product_name VARCHAR (45) NOT NULL DEFAULT "",
+    department_name VARCHAR (45) NOT NULL DEFAULT "",
     price DECIMAL (5, 2) DEFAULT 0.00,
-    stock_quantity INT NOT NULL DEFAULT 0,
+    stock_quantity INT DEFAULT 0,
     PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-    VALUES (),
-    ();
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("DC Focus snowboard", "Sports & Outdoors", 359.95, 2),
+        ("Soccer Ball", "Sporting Goods", 29.99, 30),
+        ("Basketball", "Sporting Goods", 49.99, 30),
+        ("Baseball", "Sporting Goods", 4.99, 30),
+        ("Baseball Bat", "Sporting Goods", 79.99, 15),
+        ("Volleyball", "Sporting Goods", 24.99, 10),
+        ("Lacrosse Stick", "Sporting Goods", 109.99, 20),
+        ("Golf Balls", "Sporting Goods", 49.99, 20),
+        ("Golf Club Set", "Sporting Goods", 799.99, 10),
+        ("Putter", "Sporting Goods", 134.99, 10),
+        ("Driver", "Sporting Goods", 289.99, 30),
+        ("Weight Lifting Bench", "Sporting Goods", 109.99, 5),
+        ("Squat Rack", "Sporting Goods", 219.99, 5)
+    ;
 
-SELECT * FROM bamazon_db.products;
+    SELECT *
+    FROM bamazon_db.products;
